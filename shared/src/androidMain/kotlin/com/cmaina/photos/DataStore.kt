0087ba0b5984da81585fb92f3adc7d445f9e8892
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2024-07-16 22:03:08 +0300

    Sets up datastore for desktop

--- shared/src/androidMain/kotlin/com/cmaina/photos/DataStore.kt
+++ shared/src/androidMain/kotlin/com/cmaina/photos/DataStore.kt
@@ -6,6 +6,6 @@ import androidx.datastore.preferences.core.Preferences
 import com.cmaina.photos.data.local.createDataStore
 import com.cmaina.photos.data.local.dataStoreFileName
 
-fun createDataStore(context: Context): DataStore<Preferences> = createDataStore(
+fun createAndroidDataStore(context: Context): DataStore<Preferences> = createDataStore(
     producePath = { context.filesDir.resolve(dataStoreFileName).absolutePath }
 )
--- shared/src/commonMain/kotlin/com/cmaina/photos/data/local/DataStore.kt
+++ shared/src/commonMain/kotlin/com/cmaina/photos/data/local/DataStore.kt
@@ -5,7 +5,7 @@ import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
 import okio.Path.Companion.toPath
 
-fun createDataStore(producePath: () -> String): DataStore<Preferences> =
+fun createDataStore(producePath: () -> String = { dataStoreFileName}): DataStore<Preferences> =
     PreferenceDataStoreFactory.createWithPath(produceFile = {
         producePath().toPath()
     })
