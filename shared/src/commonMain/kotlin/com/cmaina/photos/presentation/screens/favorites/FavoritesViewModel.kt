9494ad5fa365fcca66b8f99b2ed4869b9a36a4f0
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2024-08-09 18:06:20 +0300

    Creates favorites screen ui state

--- shared/src/commonMain/kotlin/com/cmaina/photos/presentation/screens/favorites/FavoritesViewModel.kt
+++ shared/src/commonMain/kotlin/com/cmaina/photos/presentation/screens/favorites/FavoritesViewModel.kt
@@ -2,9 +2,14 @@ package com.cmaina.photos.presentation.screens.favorites
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class FavoritesViewModel : ViewModel() {
+
+    private val _uiState = MutableStateFlow(FavoritesUiState())
+    val uiState: StateFlow<FavoritesUiState> get() = _uiState
 
     fun fetchFavoritePhotos() = viewModelScope.launch {
 
